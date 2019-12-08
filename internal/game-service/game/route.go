8095fbec6999ba5b191d0c63e20e4e489f2bec63
I.A <ilyas.alfan.b@gmail.com>
2019-12-08 15:46:50 +0100

    Add route finding abstraction

---
+++ internal/game-service/game/route.go
@@ -0,0 +1,7 @@
+package game
+
+// Route is the generated series of directional steps to take.
+type Route []Direction
+
+// RouteFinder calculates a path between the two given Position's.
+type RouteFinder func(grid *Grid, source, dest Position) (Route, error)
